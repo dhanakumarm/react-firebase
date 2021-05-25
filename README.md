fdc2fe945a87695f9705d65519d5e244b1683474
Dhanakumar.M <dhanakumar.m@kgisl.com>
2021-05-25 22:25:38 +0530

    commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# react-firebase
+# react-sim-space
+React sim space project
+
+## Installation and Setup Instructions
+
+#### Example:  
+
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+Installation:
+
+`npm install`  
+
+To Run Test Suite:  
+
+`npm test`  
+
+To Start Server:
+
+`npm start`  
+
+To Visit App:
+
+`localhost:3000`
---
+++ src/App.js
@@ -0,0 +1,96 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dogs</h1>
+        <div className="search">
+          <input type="text" id="search" name="search" placeholder="Search" />
+        </div>
+      </header>
+      <main>
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+
+        <div className="cards">
+          <button className="button">Button</button>
+        </div>
+      </main>
+      <footer>
+        <div className="images">
+          <div className="column">
+            <img src="https://images.dog.ceo/breeds/bluetick/n02088632_101.jpg" />
+          </div>
+
+          <div className="column">
+            <img src="https://images.dog.ceo/breeds/bluetick/n02088632_101.jpg" />
+          </div>
+          <div className="column">
+            <img src="https://images.dog.ceo/breeds/bluetick/n02088632_101.jpg" />
+          </div>
+
+          <div className="column">
+            <img src="https://images.dog.ceo/breeds/bluetick/n02088632_1047.jpg" />
+          </div>
+
+          <div className="column">
+            <img src="https://images.dog.ceo/breeds/bluetick/n02088632_101.jpg" />
+          </div>
+
+          <div className="column">
+            <img src="https://images.dog.ceo/breeds/bluetick/n02088632_1047.jpg" />
+          </div>
+        </div>
+      </footer>
+    </div>
+    // <div className="App1">
+    //   {/* <header className="App-header">
+    //     <h2>Dogs</h2>
+    //     <div>
+    //       <input type="text" id="search" name="search" placeholder="Search" />
+    //     </div>
+    //   </header> */}
+    //   <div id="header">
+    //         <p>This is the Header</p>
+    //     </div>
+    //     <div id="content">
+    //        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. </p><p>Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. </p><p>Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. </p>
+    //    </div>
+    //     <div id="footer">
+    //         <p>This is the Footer</p>
+    //     </div>
+    // </div>
+  );
+}
+
+export default App;
